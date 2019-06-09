6cfc8863fc826b0ed77db798717581551d258f39
jon91@utexas.edu <jon91@utexas.edu>
2019-06-09 13:52:32 -0700

    Final touches

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Groceries from './components/Groceries';
 import AddGrocery from './components/AddGrocery';
+import About from './components/pages/About';
 import uuid from 'uuid';
 import './App.css';
 
@@ -10,17 +12,32 @@ class App extends Component {
     groceries: [
       {
         id: uuid.v4(),
-        name: 'banana',
+        name: 'Avocados',
         purchased: false,
       },
       {
         id: uuid.v4(),
-        name: 'peanut butter',
+        name: 'Onions',
         purchased: false,
       },
       {
         id: uuid.v4(),
-        name: 'bread',
+        name: 'Chips',
+        purchased: false,
+      },
+      {
+        id: uuid.v4(),
+        name: 'Cilantro',
+        purchased: false,
+      },
+      {
+        id: uuid.v4(),
+        name: 'Tomatoes',
+        purchased: false,
+      },
+      {
+        id: uuid.v4(),
+        name: 'Jalapeños',
         purchased: false,
       }
     ]
@@ -28,12 +45,14 @@ class App extends Component {
 
   // Toggle Purchased
   togglePurchased = (id) => {
-    this.setState({ groceries: this.state.groceries.map(grocery => {
-      if(grocery.id === id) {
-        grocery.purchased = !grocery.purchased
-      }
-      return grocery;
-    }) })
+    this.setState({
+      groceries: this.state.groceries.map(grocery => {
+        if (grocery.id === id) {
+          grocery.purchased = !grocery.purchased
+        }
+        return grocery;
+      })
+    })
   }
 
   // Delete Grocery Item
@@ -47,19 +66,26 @@ class App extends Component {
       name: name,
       completed: false
     }
-    this.setState( { groceries: [...this.state.groceries, newGrocery]})
+    this.setState({ groceries: [...this.state.groceries, newGrocery] })
   }
 
   render() {
     return (
-      <div className="App">
-      <div className="container">
-        <Header />
-        <AddGrocery addGrocery={this.addGrocery}/>
-        <Groceries groceries={this.state.groceries} togglePurchased={this.togglePurchased} 
-        delGrocery={this.delGrocery} />
-      </div>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddGrocery addGrocery={this.addGrocery} />
+                <Groceries groceries={this.state.groceries} togglePurchased={this.togglePurchased}
+                  delGrocery={this.delGrocery} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Here's my little tutorial-built Grocery List web app v1.0.0.</p>
+        </React.Fragment>
+    )
+}
